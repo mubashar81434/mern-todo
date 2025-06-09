7dd919f01607b83fa3d6403d303185953066bd5c
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-09 06:50:05 +0500

    updating ../Backend/Routes/user.js

--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, signin } from "../Controllers/user.controller.js";
+import { signup, signin } from "../Controllers/user.js";
 
 const router = express.Router();
 
