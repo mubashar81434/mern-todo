d14bd5eb1e39e27182cb012929ec993f50eef6e7
mubashar81434 <mubasharsiddique81434@gmail.com>
2025-06-22 20:50:58 +0500

    updating Frontend/src/components/Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-
+import React from "react";
 const Footer = () => {
   return (
     <footer className="bg-white dark:bg-black text-center py-3 text-sm text-gray-500 dark:text-gray-400 border-t border-gray-200 dark:border-gray-700 transition-colors duration-300">
